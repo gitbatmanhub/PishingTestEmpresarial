bf763ea53a8a0dcadb51b8af8d265869e288b9e5
ecro <ecro98.98@gmail.com>
2022-08-08 22:01:02 -0500

    Envio de información del formulario metodo POST

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router= express.Router();
 
-router.get('/notes', (req, res)=>{
-    res.send("Hello madafaca from db");
+router.get('/notes/nueva', (req, res)=>{
+    res.render('notes/nueva');
 })
 
+router.post('/notes/new-note', (req, res)=>{
+    //Destructuring
+    //Creo variables con las propiedades del objeto
+    const {title, description}=req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'falta titulo'})
+    }
+    if(!description){
+        errors.push({text: 'falta descripción'})
+    }
+    if(errors.length>0){
+        res.render('notes/nueva', {
+            //datos a recuperar
+            errors,
+            title,
+            description
+        });
+    }else {
+        res.send('ok')
+
+    }
+
+})
 
 
 module.exports = router;
